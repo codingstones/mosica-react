d149f94f5238507b577687b12c09687aba5caa57
Alberto Gualis <agualis@gmail.com>
2017-11-19 19:04:08 +0100

    Extract PATH to constant

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import GigsContainer from './Days';
-import { GigDetail } from './GigDetail';
+import GigsContainer from './Days'
+import { GigDetail } from './GigDetail'
+export const GIG_DETAIL_PATH = '/gig/'
+const GIG_DETAIL_PATH_WITH_ID = '/gig/:id'
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={GigsContainer}/>
-      <Route path='/gig/:id' component={GigDetail}/>
+      <Route path={GIG_DETAIL_PATH_WITH_ID} component={GigDetail}/>
     </Switch>
   </main>
 )
--- src/services/MosicaRouter.js
+++ src/services/MosicaRouter.js
@@ -1,7 +1,9 @@
+import { GIG_DETAIL_PATH } from '../components/Main'
+
 export const MosicaRouter = (history) => {
   return { navigateToGig }
 
   function navigateToGig(gigId) {
-    history.push("/gig/" + gigId)
+    history.push(GIG_DETAIL_PATH + gigId)
   }
 }
